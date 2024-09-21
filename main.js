a773314c219eaa4fc257c270dfb9df4a768fa235
liem754 <pliem754@gmail.com>
2024-09-21 17:28:24 +0700

    fix

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-const apiKey = "d2375e24153c67f07f0747e6e45cd1d6";
-const apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=newyork";
+// const apiKey = "d2375e24153c67f07f0747e6e45cd1d6";
+// const apiUrl =
+//   "https://api.openweathermap.org/data/2.5/weather?units=metric&q=newyork";
 
-async function weather() {
-  const response = await fetch(apiUrl + `&appid=${apiKey}`);
-  const data = response.json();
-  document.querySelector(".");
-}
+// async function weather() {
+//   const response = await fetch(apiUrl + `&appid=${apiKey}`);
+//   const data = response.json();
+//   document.querySelector(".");
+// }
 
-weather();
+// weather();
